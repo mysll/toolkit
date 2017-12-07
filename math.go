40d0a7e1af43eae4cb847fa24c0d20aa913f7e54
mysll <liangliang.shi@gmail.com>
2017-12-07 10:13:04 +0800

    初始化提交

---
+++ math.go
@@ -0,0 +1,28 @@
+package toolkit
+
+import (
+	"math/rand"
+	"time"
+)
+
+func RandSeed() {
+	rand.Seed(time.Now().Unix())
+}
+
+func RandRangeI32(min int32, max int32) int32 {
+	if max < min || max-min == 0 {
+		return 0
+	}
+	return min + rand.Int31n(max-min)
+}
+
+func RandRange(min int, max int) int {
+	if max < min || max-min == 0 {
+		return 0
+	}
+	return min + rand.Intn(max-min)
+}
+
+func RandRangef(min float32, max float32) float32 {
+	return min + (max-min)*rand.Float32()
+}
---
+++ wait_group_wrap.go
@@ -0,0 +1,17 @@
+package toolkit
+
+import (
+	"sync"
+)
+
+type WaitGroupWrapper struct {
+	sync.WaitGroup
+}
+
+func (w *WaitGroupWrapper) Wrap(cb func()) {
+	w.Add(1)
+	go func() {
+		cb()
+		w.Done()
+	}()
+}
