89c24b9117ca07fe8f47641b4ed3c1a9a457405e
mysll <liangliang.shi@gmail.com>
2018-01-23 14:42:43 +0800

    更新注释

--- time.go
+++ time.go
@@ -8,6 +8,7 @@ const (
 	TIME_LAYOUT = "2006-01-02 15:04:05"
 )
 
+// UTC时间转本地时间
 func UTC2Loc(utc time.Time) time.Time {
 	y, mo, d := utc.Date()
 	h, mi, s := utc.Clock()
@@ -15,6 +16,7 @@ func UTC2Loc(utc time.Time) time.Time {
 	return t
 }
 
+// 两个时间点是否在同一天
 func IsSameDay(d1, d2 time.Time) bool {
 	return d1.Year() == d2.Year() && d1.Month() == d2.Month() && d1.Day() == d2.Day()
 }
